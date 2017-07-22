844ec3c0108ecba2c5bd294afe52a5828e0057fe
emeth- <seanybob@gmail.com>
2017-07-22 15:19:39 -0500

    fix and stub

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^get_datatable_data$', "api.views.get_datatable_data"),
     url(r'^sell$', "api.views.sell"),
+    url(r'^submit_sell$', "api.views.submit_sell"),
     url(r'^buy$', "api.views.buy"),
     url(r'^autocomplete/(?P<obj>\w+)/$', "api.views.autocomplete"),
     url(r'^$', "api.views.howitworks"),
--- api/views.py
+++ api/views.py
@@ -35,6 +35,31 @@ def get_datatable_data(request):
     }, default=json_custom_parser), content_type='application/json', status=200)
 
 
+
+def submit_sell(request):
+    #Submit it to op_return in a json structure
+    offset = int(request.GET['start'])
+    limit = int(request.GET['length'])
+
+    sort_by = request.GET["columns["+request.GET['order[0][column]']+"][name]"]
+    if request.GET["order[0][dir]"] == "desc":
+        sort_by = "-"+sort_by
+
+    c_data = Listing.objects.all().order_by(sort_by, 'block_number')
+    recordsTotal = c_data.count()
+    if request.GET.get('gamename_filter'):
+        c_data = c_data.filter(game_name__icontains=request.GET['gamename_filter'])
+    recordsFiltered = c_data.count()
+
+    return HttpResponse(json.dumps({
+        "data": list(c_data[offset:offset+limit].values()),
+        "draw": request.GET['draw'],
+        "recordsFiltered": recordsFiltered,
+        "recordsTotal": recordsTotal,
+    }, default=json_custom_parser), content_type='application/json', status=200)
+
+
+
 def buy(request):
     return TemplateResponse(request, 'buy.html', context={})
 
