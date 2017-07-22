fa879094eb6c73758ae1571808669b93d09f21f4
emeth- <seanybob@gmail.com>
2017-07-22 15:36:39 -0500

    submit listing hooked up

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ import json
 from api.models import Listing
 from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
+from OP_RETURN import *
 
 def json_custom_parser(obj):
     if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
@@ -37,28 +38,29 @@ def get_datatable_data(request):
 
 
 def submit_sell(request):
-    #Submit it to op_return in a json structure
-    offset = int(request.GET['start'])
-    limit = int(request.GET['length'])
+    """
+    "game_name": jQuery("#gamename_id").val(),
+    "currency_name": jQuery("#currency_name").val(),
+    "currency_amount": jQuery("#currency_amount").val(),
+    "cost": jQuery("#cost").val(),
+    "details": jQuery("#details").val(),
+    """
 
-    sort_by = request.GET["columns["+request.GET['order[0][column]']+"][name]"]
-    if request.GET["order[0][dir]"] == "desc":
-        sort_by = "-"+sort_by
+    data = {
+        "a": "pl", #App = Peerlistings
+        "gn": request.POST['game_name'],
+        "cn": request.POST['currency_name'],
+        "ca": request.POST['currency_amount'],
+        "c": request.POST['cost'],
+        "d": request.POST['details'],
+    }
 
-    c_data = Listing.objects.all().order_by(sort_by, 'block_number')
-    recordsTotal = c_data.count()
-    if request.GET.get('gamename_filter'):
-        c_data = c_data.filter(game_name__icontains=request.GET['gamename_filter'])
-    recordsFiltered = c_data.count()
+    results = OP_RETURN_store(json.dumps(data), testnet=True)
+    print "Stored in blockchain, results: ", results
 
     return HttpResponse(json.dumps({
-        "data": list(c_data[offset:offset+limit].values()),
-        "draw": request.GET['draw'],
-        "recordsFiltered": recordsFiltered,
-        "recordsTotal": recordsTotal,
+        "results": results,
     }, default=json_custom_parser), content_type='application/json', status=200)
-
-
 
 def buy(request):
     return TemplateResponse(request, 'buy.html', context={})
